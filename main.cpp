11696af5a18f0efe0cf21bb9a1603e7024f00ea6
rstefan123 <stefanrichter123@gmail.com>
2020-04-09 22:39:29 +0000

    completed assignment

--- main.cpp
+++ main.cpp
@@ -17,7 +17,12 @@ video: Chapter 2 - Part 3
  1) Write down the names of all of the primitives available in C++ (excluding wchar_t)
  put them here: 
  
- 
+ int
+ char 
+ float
+ bool
+ double
+ void
  
  
  
@@ -33,12 +38,44 @@ video: Chapter 2 - Part 3
         'void' is a return type. you do not need to declare 3 variables of type 'void'.
     at the end of the function, pass each variable to the ignoreUnused function
  
+ */
+
+void variableDeclarations()
+{
+    int myAge = 25;
+    int myBirthday = 92294;
+    int currentTemperature = 75;
+
+    char myFirstInitial = 'S';
+    char myLastInitial = 'R';
+    char myFriendsInitial = 'R';
+
+    float myExactAge = 25.5f;
+    float proLineLevel = 1.228f;
+    float consumerLineLevel= .316f;
+
+    bool sittingDown = true;
+    bool standingUp = false;
+    bool havingFun = true;
+
+    double myNumber = 1.234567891;
+    double yourNumber = 9.87654321987654321;
+    double guitarCap = 0.0000022;
+
+    ignoreUnused(myAge,myBirthday,currentTemperature,myFirstInitial,myLastInitial,myFriendsInitial,myExactAge,proLineLevel,consumerLineLevel, sittingDown,standingUp, havingFun,myNumber,yourNumber,guitarCap);
+}
+
+
+
+
+/*
 3) write out 10 functions
     each declaration should have a random number of parameters in the function parameter list.
     add { ignoreUnused( ); } after each declaration in place of the closing semicolon
     pass each of your function parameters to the ignoreUnused function. 
     if your function returns something other than void, add 'return {};' at the end of it.
     
+
 4) provide default values for an arbitrary number of parameters in the function parameter list.
     When naming your parameters, choose names that are relevant to the task implied by the function's name.
     
@@ -61,6 +98,7 @@ send me a DM to check your pull request
  */
 
 //2)
+/*
 void variableDeclarations()
 {
     //example:
@@ -70,7 +108,7 @@ void variableDeclarations()
     
     ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
 }
-/*
+
  10 functions
  example:
  */
@@ -83,42 +121,99 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 /*
  1)
  */
-
+double fileTaxReturn(bool W2, bool TenNinetyNine, float income)
+{
+ ignoreUnused(W2, TenNinetyNine, income);
+    return {};
+}
 /*
  2)
  */
-
+bool waterBoiling(float temperature, bool boilingPoint)
+{
+    ignoreUnused(temperature, boilingPoint);
+    return {};
+}
 /*
  3)
  */
-
+double measureVoltage(float Voltage)
+{
+    ignoreUnused(Voltage);
+    return {};
+}
 /*
  4)
  */
-
+bool checkIfHungry(float timeLastEaten, bool stomachGrowl, bool hungry)
+{
+    ignoreUnused(timeLastEaten, stomachGrowl, hungry);
+    return {};
+}
 /*
  5)
  */
-
+bool checkIfTired(float timeWokenUp, int hoursSlept)
+{
+    ignoreUnused(timeWokenUp, hoursSlept);
+    return {};
+}
 /*
  6)
  */
-
+float measureCap(double capacitance, double ESR, bool shortCircuit, bool open)
+{
+    ignoreUnused(capacitance, ESR, shortCircuit, open);
+    return {};
+}
 /*
  7)
  */
-
+bool finishCourse(int lessonsCompleted, bool allLessonsDone)
+{
+    ignoreUnused(lessonsCompleted, allLessonsDone);
+    return {};
+}
 /*
  8)
  */
-
+bool learnSpanish(int lessonsCompleted, bool fluent)
+{
+    ignoreUnused(lessonsCompleted, fluent);
+    return {};
+}
 /*
  9)
  */
-
+int countCoffeesDrank(bool morningCup, bool afternoonCup, int extraCups)
+{
+    ignoreUnused(morningCup, afternoonCup, extraCups);
+    return {};
+}
 /*
  10)
  */
+bool checkAmp(bool tubes, bool caps, bool hardware)
+{
+    ignoreUnused(tubes, caps, hardware);
+    return {};
+}
+
+/* default values for functions step 4 */
+
+void variableDeclarations2()
+{
+    double capacitance = .0000022;
+    float temperature = 100;
+    bool W2 = true;
+    float voltage = 3.16f;
+
+    ignoreUnused(capacitance, temperature, W2, voltage);
+}
+
+
+
+
 
 int main()
 {
@@ -126,27 +221,29 @@ int main()
     auto carRented = rentACar(6, 2); 
     
     //1)
-    
+    auto taxReturn = fileTaxReturn(true, false, 75000);
     //2)
-    
+    auto isWaterBoiling = waterBoiling(100, 0);
     //3)
-    
+    auto measuredVoltage = measureVoltage(3.16f);
     //4)
-    
+    auto isHungry = checkIfHungry(12, 1, 1);
     //5)
-    
+    auto isTired = checkIfTired(7, 6);
     //6)
-    
+    auto capacitance = measureCap(.0000022, 1, 0, 0);
     //7)
-    
+    auto courseFinished = finishCourse(2, 0);
     //8)
-    
+    auto spanishLearned = learnSpanish(5, 0);
     //9)
-    
+    int coffeesDrank = countCoffeesDrank(1, 1, 1);
     //10)
-    
+    bool ampOk = checkAmp(1, 1, 1);
+
+    ignoreUnused(taxReturn,isWaterBoiling,measuredVoltage,isHungry,isTired,capacitance,courseFinished,spanishLearned,coffeesDrank,ampOk);  
     
     ignoreUnused(carRented);
     std::cout << "good to go!" << std::endl;
-    return 0;    
+    return 0;  
 }
